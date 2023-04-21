ec2489501e576307d652444c6eba236aeab9e2ff
zhuima <zhuima314@gmail.com>
2023-04-21 17:10:42 +0800

    update logo and headers

--- src/components/Narbars.tsx
+++ src/components/Narbars.tsx
@@ -1,80 +1,77 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import logo from "/assets/logo2.png";
 
 export const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
   return (
-    <>
-      <nav className="relative flex flex-wrap items-center justify-between px-2 bg-indigo-500 mb-3">
-        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-          <div className="w-full relative flex justify-between lg:w-auto  lg:static lg:block lg:justify-start">
-            <Link to={`/`}>
-              <h1 className="side-animated-heading">
-                <span className="text-white">软考系规</span>
-                <div className="nav-side-message-box">
-                  <div className="side-box-animated-word1">资料库</div>
-                  <div className="side-box-animated-word2">加油站</div>
-                  <div className="side-box-animated-word3">学习方法</div>
-                </div>
-              </h1>
-            </Link>
-            <button
-              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
-              type="button"
-              onClick={() => setNavbarOpen(!navbarOpen)}
+    <div className="bg-white shadow" id="navbar-cta">
+      <nav
+        className="group container relative mx-auto flex items-center justify-between space-x-4 py-5 px-3"
+        id="navbar"
+      >
+        <a
+          href="https://www.ruankao168.top"
+          className="flex items-center justify-start space-x-2"
+          id="logo"
+        >
+          <img src={logo} alt="LOGO" className="block w-10 object-cover" />
+          <h2 className="font-['Red Hat Text'] font-bold text-center text-lg">
+            系规通关宝典
+          </h2>
+        </a>
+        <ul
+          id="navbar-menu"
+          className="absolute right-1 top-[4.05rem] z-10 hidden w-full flex-col items-center justify-center bg-white px-3 shadow-md group-hover:flex md:static md:flex md:w-auto md:flex-row md:space-y-0 md:space-x-4 md:shadow-none"
+        >
+          <li className="px-3 py-1 md:p-0">
+            <a
+              href="https://www.ruankao168.top/course"
+              className="px-3 py-2 flex items-center text-2xs uppercase font-bold leading-snug  hover:opacity-75"
             >
-              <i className="fas fa-bars"></i>
-            </button>
-          </div>
-          <div
-            className={
-              "lg:flex flex-grow items-center" +
-              (navbarOpen ? " flex" : " hidden")
-            }
-            id="example-navbar-danger"
-          >
-            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
-              <li className="nav-item">
-                <Link
-                  to={`/course`}
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                >
-                  <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i>
-                  <span className="ml-4">课程计划</span>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  target="_blank"
-                  to={`https://www.bilibili.com/video/BV1Ce411N7pV/`}
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                >
-                  <i className="fab fa-twitter text-lg leading-lg text-white opacity-75"></i>
-                  <span className="ml-2">B站视频</span>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  to={`/document`}
-                >
-                  <i className="fab fa-pinterest text-lg leading-lg text-white opacity-75"></i>
-                  <span className="ml-2">学习指北</span>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  to={`/books`}
-                >
-                  <i className="fab fa-pinterest text-lg leading-lg text-white opacity-75"></i>
-                  <span className="ml-2">教材参考</span>
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
+              课程计划
+            </a>
+          </li>
+          <li className="px-3 py-1 md:p-0">
+            <a
+              href="https://www.bilibili.com/video/BV1Ce411N7pV/"
+              className="px-3 py-2 flex items-center text-2xs uppercase font-bold leading-snug  hover:opacity-75"
+            >
+              B站视频
+            </a>
+          </li>
+          <li className="px-3 py-1 md:p-0">
+            <a
+              href="https://www.ruankao168.top/document"
+              className="px-3 py-2 flex items-center text-2xs uppercase font-bold leading-snug  hover:opacity-75"
+            >
+              学习指北
+            </a>
+          </li>
+          <li className="px-3 py-1 md:p-0">
+            <a
+              href="https://www.ruankao168.top/books"
+              className="px-3 py-2 flex items-center text-2xs uppercase font-bold leading-snug  hover:opacity-75"
+            >
+              教材参考
+            </a>
+          </li>
+        </ul>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 md:hidden"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
       </nav>
-    </>
+    </div>
   );
 };
