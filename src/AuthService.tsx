9cfd811d9d6362d5c0fe9a8acf18a10daba15663
zhuima <zhuima314@gmail.com>
2023-04-21 13:48:42 +0800

    repair ts synax

--- src/AuthService.tsx
+++ src/AuthService.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-export default function VerificationCode(props) {
+export default function VerificationCode(props: any) {
   const [showContent, setShowContent] = useState(false);
   const [inputValue, setInputValue] = useState("");
   const [error, setError] = useState(false);
 
-  const handleInput = (e) => {
+  const handleInput = (e: any) => {
     setInputValue(e.target.value);
   };
 
@@ -33,12 +33,14 @@ export default function VerificationCode(props) {
               回复<span className="text-red-500">软考</span>获取验证码
             </p>
             <div className="border border-gray-300 rounded-md p-4 mb-4">
-              <img src={props.image} alt="验证码" className="mx-auto" />
+              <img
+                src={props.image}
+                alt="验证码"
+                className="mx-auto"
+                loading="lazy"
+              />
             </div>
             <form onSubmit={handleSubmit} className="flex items-center">
-              <span className="z-10 h-full leading-snug font-normal absolute text-center text-slate-300 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3">
-                <i className="fas fa-lock"></i>
-              </span>
               <input
                 type="text"
                 placeholder="请输入验证码"
@@ -46,6 +48,7 @@ export default function VerificationCode(props) {
                 onChange={handleInput}
                 className="rounded-md border border-gray-400 p-2 mr-2 flex-1 w-full"
               />
+
               <button
                 type="submit"
                 className="bg-blue-400 text-white px-4 py-2 rounded-md  hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50"
